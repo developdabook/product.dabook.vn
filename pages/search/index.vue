b2155cb304e681de62ee2adff50e59f9f35bc9fe
developdabook <sysadmin@dabook.vn>
2020-02-20 19:11:52 +0700

    Them stickysearch

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -1,9 +1,28 @@
 <template>
-  <div class="search-page"></div>
+  <div class="search-page">
+    <section class="section-stickysearch">
+      <StickySearch />
+    </section>
+    <section class="section-search-result tw-container tw-mx-auto">
+      <div class="tw-w-full md:tw-w-3/5 tw-mx-auto">
+        <Ticket v-for="i in 10" :key="i" class="tw-mb-2" />
+      </div>
+    </section>
+  </div>
 </template>
 <script>
 export default {
   name: 'SearchPage',
-  layout: 'unauthen'
+  layout: 'unauthen',
+  components: {
+    StickySearch: () => import('@/components/search/StickySearch'),
+    Ticket: () => import('@/components/select/Ticket')
+  }
 }
 </script>
+<style lang="postcss">
+.section-stickysearch {
+  z-index: 2;
+  @apply tw-sticky tw-top-0;
+}
+</style>
